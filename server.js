1f2426e99233a257eba079bb2153d6d43e903190
Sarah Payne <sarah@Sarahs-MacBook-Pro.local>
2017-07-29 22:26:00 -0500

    almost complete javascript functionality

--- server.js
+++ server.js
@@ -5,17 +5,11 @@ var express = require("express"),
 var app = express();
 var PORT = process.env.PORT || 8080;
  
-app.use(bodyParser.urlencoded({ extended: false }))
- 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
- 
-// app.use(function (req, res) {
-//   res.setHeader('Content-Type', 'text/plain')
-//   res.write('you posted:\n')
-//   res.end(JSON.stringify(req.body, null, 2))
-// })
 
-require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);

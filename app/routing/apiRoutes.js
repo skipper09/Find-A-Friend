be4de8787b7a687483041e11c52194ac59bafd46
Sarah Payne <sarah@Sarahs-MacBook-Pro.local>
2017-07-29 23:37:34 -0500

    working on comparison logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,36 @@
-var friendsData = require("./../data/friends.js");
+var friendsArray = require("./../data/friends.js");
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
-        res.json(friendsData);
+        res.json(friendsArray);
     });
 
     app.post("/api/friends", function(req, res) {
-        friendsData.push(req.body);
+        var newFriend = req.body;
+        var yourMatch;
+        var difference = [];
+
+        // friendsData.push(req.body);
+
+        //compare with each friends data
+
+        for (var i=0; i<friendsArray.length; i++) {
+            for (var j=0;j<friendsArray[i].scores.length; j++) {
+                difference.push(Math.abs(newFriend.scores[j] - friendsArray[i].scores[j]))
+            }
+
+
+
+
+        }
+
+        res.json(difference);
+
+        // find friend with least difference
+
+        //modal pop up with dude
+
+
     })
 }
+
